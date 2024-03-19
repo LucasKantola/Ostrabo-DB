e877630c6827276fe43e8d70cbce43d8caa2021c
Lucas-kantola <Kantola05@gmail.com>
2024-03-19 17:44:58 +0100

    styling and structure changes

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Östrabo DB</title>
+    <link rel="stylesheet" href="https://use.typekit.net/skk6qik.css">
     <link rel="stylesheet" href="{{asset("style/landing.css")}}">
+    <link rel="stylesheet" href="{{asset("style/defaults.css")}}">
 </head>
 <body>
     <header>
@@ -12,22 +14,28 @@
             <img src="{{ asset('Uddevalla gymnasieskola logotyp.png') }}" alt="Uddevalla Gymnasieskola logotyp">
             <img src="{{ asset('O1-logo.png') }}" alt="Östrabo1 Uddevalla Gymnasieskola logotyp">
         </div>
+        <nav>
+            <ul>
+                <li><a href="{{ route('home') }}">Home</a></li>
+                <li><a href="{{ url('/browse') }}">Browse</a></li>
+                <li><a href="#">Add Work</a></li>
+            </ul>
+        </nav>
     </header>
     <main>
+        <h1 class="welcome">Välkommen till Östrabo DB</h1>
+        <h2></h2>
         <div class="search-bar">
-
+            <input type="text" placeholder="Search...">
+            <button>Search</button>
         </div>
         <div class="browse-options">
-            <button>Browse</button>
-            <button>Websites</button>
-            <button>Games</button>
-            <button>Practical</button>
+            <button>Utforska</button>
+            <button>Praktiska Arbeten</button>
         </div>
-        <!-- Document listings can go here -->
     </main>
     <footer>
-        <p>&copy; 2024 Lucas Kantola TE21B</p>
+        <p class="center bottom">&copy; 2024 Lucas Kantola TE21B</p>
     </footer>
-    <!-- Include any JS files here -->
 </body>
 </html>
