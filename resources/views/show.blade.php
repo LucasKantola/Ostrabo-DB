1d3c28dedf0daab235bbdb08b5b04bc0ac10b673
Lucas-kantola <Kantola05@gmail.com>
2024-05-07 17:19:22 +0200

    added some elements to the view page

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,8 +3,28 @@
 </head>
 <body>
     @include('../defaults/header')
-    <main>
-        
+    <main class="h100 flex-column">
+        <div class="left">
+                <div class="titles">
+                <h2>{{$data->title}}</h2>
+                <h4>{{$data -> author}}</h4>
+                <time datetime="{{$data -> publication_date}}">{{$data -> publication_date}}</time>
+            </div>
+        </div>
+        <div class="right">
+            @forelse(explode(",", $data->keywords) as $keyword)
+                <span class="tag">{{$keyword}}</span>
+            @empty
+                <span class="tag"></span>
+            @endforelse
+        </div>
+
+        <div class="abstract">
+            <h3>Abstract:</h3>
+            <p class="abstract-text">
+                {{$data->abstract}}
+            </p>   
+        </div>
     </main>
     @include('../defaults/footer')
 </body>
